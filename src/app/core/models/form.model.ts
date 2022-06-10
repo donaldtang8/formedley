38c6a6ab9b73b94cc4780f448c676d4bfacca66d
Donald Tang <donaldtang08@gmail.com>
2022-06-10 03:01:10 -0400

    Refactored code to better handle validation of form from child form to parent form

--- src/app/core/models/form.model.ts
+++ src/app/core/models/form.model.ts
@@ -1,7 +1,8 @@
 import { FormInput } from './form-input.model';
 
 export class Form {
-    constructor(public inputs: FormInput[]) {
+    constructor(public title: string, public inputs: FormInput[]) {
+        this.title = title;
         this.inputs = inputs;
     }
 }
