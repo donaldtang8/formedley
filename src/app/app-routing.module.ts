9e57972aec738e9952b01478db03aa523ce49f4d
Donald Tang <donaldtang08@gmail.com>
2022-06-12 03:24:15 -0400

    Added functionality to store and fetch forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/auth', pathMatch: 'full' },
+    { path: '', redirectTo: '/forms', pathMatch: 'full' },
     { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
     { path: 'forms', loadChildren: () => import('./features/forms/forms.module').then(m => m.CustomFormsModule) }
 ];
--- src/app/core/models/form.model.ts
+++ src/app/core/models/form.model.ts
@@ -1,8 +1,9 @@
 import { FormInput } from './form-input.model';
 
 export class Form {
-    constructor(public title: string, public inputs: FormInput[]) {
+    constructor(public title: string,  public userId: string, public inputs: FormInput[]) {
         this.title = title;
+        this.userId = userId;
         this.inputs = inputs;
     }
 }
